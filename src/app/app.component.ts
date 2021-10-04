498eebb2eb21c12de53ffd814cb2f7a5fc127bad
darriola7 <darriola710@gmail.com>
2021-10-04 15:01:14 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './register/register.component.html',
-  styleUrls: ['./login/login.component.css']
+  templateUrl: './components/register/register.component.html',
+  styleUrls: ['./components/login/login.component.css'],
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
